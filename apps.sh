e0078992a971e8fc9b359a3d26df222790cd3948
mooseman645 <m00seman645@gmail.com>
2017-10-30 15:08:16 -0400

    1

---
+++ apps.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# login as root
+sudo su
+
+# install repos
+sudo add-apt-repository ppa:rebuntu16/other-stuff 
+sudo add-apt-repository ppa:notepadqq-team/notepadqq
+sudo add-apt-repository ppa:libreoffice/libreoffice-5-4
+sudo add-apt-repository ppa:embrosyn/cinnamon
+sudo add-apt-repository ppa:nilarimogard/webupd8
+=======
+
+>>>>>>> parent of 57c7edc... update
+# update apt
+sudo apt-get update
+
+# Install apps
+sudo apt-get install xfce-theme-manager notepadqq git cinnamon
+sudo apt install --install-recommends avant-window-navigator
+sudo apt install --no-install-recommends awn-applets-all
+=======
+sudo apt-get install xfce-theme-manager notepadqq git
+>>>>>>> parent of 57c7edc... update
+
+# install katoolin
+# git clone https://github.com/LionSec/katoolin.git && cp katoolin/katoolin.py /usr/bin/katoolin
+#sudo chmod +x /usr/bin/katoolin
+
